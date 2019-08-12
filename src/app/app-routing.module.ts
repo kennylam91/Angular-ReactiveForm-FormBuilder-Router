15c4f411743c0ec12eb45e2b27c52fc269eafd09
kennylam91 <phamvanlam1991@gmail.com>
2019-08-12 09:03:09 +0700

    add button and router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {NameEditorComponent} from './name-editor/name-editor.component';
+import {ProfileEditorComponent} from './profile-editor/profile-editor.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'name-editor', component: NameEditorComponent},
+  {path: 'profile-editor', component: ProfileEditorComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
